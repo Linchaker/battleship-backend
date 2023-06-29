8fbf87223569ad9604531642b18f14d78102a8d1
Linchaker <linchaker@gmail.com>
2023-06-29 23:02:16 +0300

    init oponent board

--- src/controllers/api/PlayController.js
+++ src/controllers/api/PlayController.js
@@ -1,3 +1,4 @@
+const { mongoose } = require("mongoose")
 const Board = require("../../models/Board")
 const Game = require("../../models/Game")
 const BoardService = require("../../services/board-service")
@@ -36,6 +37,55 @@ class PlayController {
     } catch (e) {
       console.log(e)
       res.status(400).json({message: 'Create game error'})
+    }
+  }
+
+  async getGame(req, res) {
+    try {
+      let gameId = req.params.gameId
+  
+      if (!gameId || !mongoose.Types.ObjectId.isValid(gameId)) {
+        res.status(404).json({message: 'Game not found'})
+        return
+      }
+
+
+      const game = await Game.findById(gameId);
+
+      if (!game) {
+        res.status(404).json({message: 'Game not found'})
+        return
+      }
+
+
+      if (req.user._id !== game.ownerUserId && !game.oponentUserId) {
+        game.oponentUserId = req.user._id
+
+        const oponentBoard = new BoardService(new Board(10))
+        oponentBoard.createFleet()
+
+        game.data.oponentData = {
+          board: oponentBoard.getBoard(),
+          fleet: oponentBoard.getFleet()
+        }
+
+        await game.save()
+      }
+
+       // создвать если нету для опонента доску и флот
+       // в дальнейшем можено будет вырезать флот противника, доску оставляем чтобы показать куда уже стреляли
+       // и добавить хитам статус попадания, либо одельный массив подбитых кораблей
+
+      console.log(game);
+
+      res.json({
+        "success": true,
+        "game": game,
+      })
+      
+    } catch (e) {
+      console.log(e)
+      res.status(400).json({message: 'Get game error'})
     }
   }
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -13,7 +13,8 @@ router.post('/auth/login', AuthController.login);
 router.post('/auth/register', registerValidators, AuthController.register);
 
 
-router.get('/play/create', authMiddleware, PlayController.createGame);
+router.post('/play', authMiddleware, PlayController.createGame);
+router.get('/play/:gameId?', authMiddleware, PlayController.getGame);
 // temp without middleware router.get('/play/create', PlayController.createGame);
 
 module.exports = router;
